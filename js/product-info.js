a4da0011c68fe35b4a357d1b3455706b11bcb6bb
Karen Montero <karen.monterourru@gmail.com>
2020-09-12 12:16:21 -0300

    pre Entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -20,26 +20,9 @@ function showImagesGallery(array){
         document.getElementById("productImages").innerHTML = htmlContentToAppend;
     }
 }
-//function showRating() {
-//    let score = product.rating;
-//    let stars = "";
-//    let html = "";
-//    for (let i = 1; i <= maxProductRating; i++) {
-//        if (i <= score) {
-//            stars += '<i class="fa fa-star checked"></i>';
-//        } else {
-//            stars += '<i class="fa fa-star"></i>';
-//        }
-//    }
 
-//    html = `<span>  ${stars}</span>`
-
-//    document.getElementById("rating").innerHTML = html;
-
-//}
-
-function showRelatedProducts(relatedProductArray) {
-    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+ function showRelatedProducts(relatedProductArray) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             productList = resultObj.data;
 
@@ -51,22 +34,77 @@ function showRelatedProducts(relatedProductArray) {
 
                 htmlRelatedProduct += `
                 <div class= "col-lg-3 col-md-4 col-6 border">
-                    <div id="relatedVideogameImg" class= "row">
-                        <img class="img-fluid p-2" src="`+relatedProduct.images[0]+`">                                              
+                    <div id="relatedProductImg" class= "row">
+                        <img class="img-fluid p-2" src="`+ relatedProduct.imgSrc +`">                                              
                     </div>                   
-                    <div "relatedVideogameInfo" class= "row p-2">
+                    <div "relatedProductInfo" class= "row p-2">
                     <p>`+ relatedProduct.name + `</p> 
                     <p>`+ relatedProduct.description + `</p>
                     </div>
                     <div class= "row p-2">
-                    <a href="videogames-info.html">Ver</a>
+                    <a href="product-info.html">Ver</a>
                     </div>                     
                 </div>`
             }
             document.getElementById("relatedProductContainer").innerHTML = htmlRelatedProduct;
         }
-    })
+    });
+};
+
+
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+           let comment = resultObj.data;
+           
+           let htmlContentToAppend = "";
+           comment.forEach(function(comment){
+            let commentScore = comment.score;
+            let score = '';
+            for (let i = 1; i <= commentScore; i++) {
+                score += '<i class="fas fa-star checked"></i>';
+            }
+            
+            for (let i = commentScore +1; i <= commentScore; i++) {
+                score += '<i class="far fa-star"></i>';
+            }
+
+            htmlContentToAppend += `
+            <li class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">`+ comment.user +`</h5>
+                         <small class="valoracion">` + score + `</small>
+                    </div>
+                     <p class="mb-1"><label>` + comment.description + `</label></p>
+                     <p class="text-muted">` + comment.dateTime + `</p>
+                    </div>
+                </div>
+             `
+            });
+            document.getElementById("comments").innerHTML = htmlContentToAppend;
+        }
+    });
+
+
+        
+ 
+function newcomment(comentario){
+//     if ((localStorage.comentario != undefined) && (localStorage.calificacion != undefined)) {
+//         document.getElementsByClassName("comentario").innerHTML = "Nombre: " + localStorage.nombre;
+//         document.getElementsByClassName("input password").innerHTML= " Password: " + localStorage.password;
+//     } else {
+//         document.getElementsByClassName("input password").innerHTML = "No has introducido tu contraseña";
+//     }
+// }
+    localStorage.comentario = document.getElementsByClassName("comentario").value;
+    localStorage.calificacion = document.getElementsByClassName("calificacion").value;
+    alert ("Su comentario a sido enviado con exito");
+    document.getElementById("comentario").value="";
 }
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -81,17 +119,20 @@ document.addEventListener("DOMContentLoaded", function(e){
             let productDescriptionHTML = document.getElementById("productDescription");
             let productCurrencyHTML = document.getElementById("productCurrency");
             let productCostHTML = document.getElementById("productCost");
+            let productCategoryHTML = document.getElementById("productCategory");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
         
-            productNameHTML = product.name;
-            productDescriptionHTML = product.description;
-            productCurrencyHTML = product.currency;
-            productCostHTML = product.cost;
-
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCurrencyHTML.innerHTML = product.currency;
+            productCostHTML.innerHTML = product.cost;
+            productCategoryHTML.innerHTML = product.category;
+            productSoldCountHTML.innerHTML = product.soldCount;
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
-//            showRating();
-            showRelatedProducts(product.relatedProduct);
         }
-    });
+        
+        showRelatedProducts(product.relatedProducts);
 
+    });
 });
